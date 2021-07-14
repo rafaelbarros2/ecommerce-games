d972e486c563790bcc05c6abc379d11ac308deb9
Rafael Barros <rafael.eng.mecanic@gmail.com>
2021-07-14 11:07:09 -0300

    service, component registration

--- src/main/java/br/com/supera/game/controller/ProductController.java
+++ src/main/java/br/com/supera/game/controller/ProductController.java
@@ -1,8 +1,10 @@
 package br.com.supera.game.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value = "/product")
 public class ProductController {
 	
 
